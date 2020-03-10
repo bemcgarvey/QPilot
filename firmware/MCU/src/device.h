b7e5f65738acebd042e750695a84b0b675f7c47f
bemcgarvey <bemcgarvey@comcast.net>
2020-03-10 14:47:19 -0600

    Moved firmware to its own repository

--- firmware/MCU/src/device.h
+++
@@ -1,9 +0,0 @@
-//Common include file - use in all source files
-
-#include <xc.h>
-#include <sys/attribs.h>
-#include "toolchain_specifics.h"
-#include <stddef.h>             
-#include <stdbool.h>                   
-#include <stdint.h>
-
